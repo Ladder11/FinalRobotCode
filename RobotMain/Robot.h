7614c8b5f8eae93c8ac74b2e9427bc3501e53185
engineeringawesome <robomower9508@gmail.com>
2015-12-06 21:17:28 -0500

    Added conditional compilation, and included constants file

---
+++ RobotMain/Robot.h
@@ -0,0 +1,12 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+/**
+ * Singleton Robot object for accessing various
+ * Robot subsystems
+ * @author Jordan Burklund
+**/
+#include "RobotConstants.h"
+
+
+#endif
