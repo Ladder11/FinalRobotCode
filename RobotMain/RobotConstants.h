7e9875340e5437c77273ebf889136e25eca91fd7
engineeringawesome <robomower9508@gmail.com>
2015-12-06 21:10:24 -0500

    Created, and added Ultrasonic sensor pins

---
+++ RobotMain/RobotConstants.h
@@ -0,0 +1,11 @@
+/**
+ * Constants for the robot, like pin numbers, scale factors, etc.
+ **/
+
+//Ultrasonic Sensor Pins
+const int US_RIGHT_INPUT_PIN = 24;
+const int US_RIGHT_OUTPUT_PIN = 25;
+const int US_FRONT_INPUT_PIN = 26;
+const int US_FRONT_OUPUT_PIN = 27;
+const int US_LEFT_INPUT_PIN = 28;
+const int US_LEFT_OUTPUT_PIN = 29;
