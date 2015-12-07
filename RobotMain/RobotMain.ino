36a484f31b32f7789328bacf22b4916e4cee3caa
engineeringawesome <robomower9508@gmail.com>
2015-12-06 21:55:30 -0500

    Added scheduler and robot initialization

--- RobotMain/RobotMain.ino
+++ RobotMain/RobotMain.ino
@@ -1,10 +1,21 @@
+/** Main file that will be executed on the robot
+  * for the RBE2002 Final Project
+  * @author Jordan Burklund
+  * @author Hans Johnson
+  * @author Garrison Hefter
+  **/
 
 #include "Robot.h"
+#include "Scheduler.h"
+
+Scheduler* scheduler = Scheduler::getInstance();
+Robot* ladder11 = Robot:: getInstance();
 
 void setup() {
-
+  ladder11->initializeSubsystems();
 }
 
 void loop() {
+  scheduler->run();
 
 }
