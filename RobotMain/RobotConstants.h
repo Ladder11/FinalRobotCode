a7262feea0e2c08cdbd9632584e20ea5e033c868
engineeringawesome <robomower9508@gmail.com>
2015-12-06 21:14:19 -0500

    Added conditional definitions

--- RobotMain/RobotConstants.h
+++ RobotMain/RobotConstants.h
@@ -2,6 +2,9 @@
  * Constants for the robot, like pin numbers, scale factors, etc.
  **/
 
+#ifndef ROBOTCONSTANTS_H
+#define ROBOTCONSTANTS_H
+
 //Ultrasonic Sensor Pins
 const int US_RIGHT_INPUT_PIN = 24;
 const int US_RIGHT_OUTPUT_PIN = 25;
@@ -9,3 +12,5 @@ const int US_FRONT_INPUT_PIN = 26;
 const int US_FRONT_OUPUT_PIN = 27;
 const int US_LEFT_INPUT_PIN = 28;
 const int US_LEFT_OUTPUT_PIN = 29;
+
+#endif
