a876308e79d528e0c8df8246c94078313d6df8d8
hejohnson <hejohnson@wpi.edu>
2015-12-08 18:03:32 -0500

    Moved code around for debugging

--- RobotMain/ApproachFlame.cpp
+++ RobotMain/ApproachFlame.cpp
@@ -9,15 +9,21 @@ void ApproachFlame::initialize() {
 }
 
 void ApproachFlame::execute() {
-	distSpeed = .25*(ladder11->frontSensor->distance()-20);
+  if (ladder11->flameSense->isFlame()) {
+    distSpeed = .25*(ladder11->frontSensor->distance()-20);
     if (distSpeed > 5) {
-      distSpeed = 5;
+    distSpeed = 5;
     }
     if (distSpeed < 1.5) {
-      distSpeed = 1.5;
+     distSpeed = 1.5;
     }
     ladder11->drivetrain->drive(distSpeed, ladder11->flameSense->flameAngle()*180/3.1415*4);
-    ladder11->drivetrain->updateRobotPos();
+  } else {
+    ladder11->drivetrain->drive(0, 50);
+  }
+  ladder11->drivetrain->updateRobotPos();
+  ladder11->lcd->clear();
+  ladder11->lcd->print(ladder11->frontSensor->distance(), DEC);
 }
 
 bool ApproachFlame::isFinished() {
@@ -25,6 +31,6 @@ bool ApproachFlame::isFinished() {
 }
 
 void ApproachFlame::end() {
-	ladder11->lcd->print("Flame aheado");
+	ladder11->lcd->print("Flame ahead");
 	ladder11->drivetrain->stop();
 }
