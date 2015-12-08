98fd2d1d5df449915e448f13fffcbdecfac5b849
hejohnson <hejohnson@wpi.edu>
2015-12-07 19:45:26 -0500

    Added command

--- RobotMain/RobotMain.ino
+++ RobotMain/RobotMain.ino
@@ -8,12 +8,14 @@
 #include "Robot.h"
 #include "Scheduler.h"
 #include "Drivetrain.h"
+#include "WallFollow.h"
 
 Scheduler* scheduler = Scheduler::getInstance();
 Robot* ladder11 = Robot::getInstance();
 
 void setup() {
   ladder11->initializeSubsystems();
+  scheduler->addCommand(new WallFollow(8.0));
 }
 
 void loop() {
