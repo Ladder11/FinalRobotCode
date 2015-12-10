d4a23f018c59cdae487a004a9b3ceb3da92620a6
engineeringawesome <robomower9508@gmail.com>
2015-12-10 12:00:19 -0500

    Added more commands

--- RobotMain/Ladder11TelemetryConstants.h
+++ RobotMain/Ladder11TelemetryConstants.h
@@ -13,3 +13,7 @@ const uint8_t COMMAND_ROBOT_POSE = 0x03;
 const uint8_t COMMAND_FLAME_LOC = 0x04;
 const uint8_t COMMAND_CAMERA_DATA = 0x05;
 const uint8_t COMMAND_BATT_VOLT = 0x06;
+const uint8_t COMMAND_GYRO_DATA = 0x07;
+const uint8_t COMMAND_STATUS = 0x08;
+const uint8_t COMMAND_RUNNING = 0x09;
+const uint8_t COMMAND_STOPPED = 0x0A;
