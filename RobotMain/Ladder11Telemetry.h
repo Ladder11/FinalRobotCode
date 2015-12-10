e5b00cd5e08a77ff82d682434f5b75d371298ca2
engineeringawesome <robomower9508@gmail.com>
2015-12-10 12:05:45 -0500

    Added more sending methods

--- RobotMain/Ladder11Telemetry.h
+++ RobotMain/Ladder11Telemetry.h
@@ -12,6 +12,13 @@ class Ladder11Telemetry {
 public:
 	Ladder11Telemetry();
 	void sendRobotPose(float x, float y, float theta);
+	void sendFlameLoc(float x, float y, float z);
+	void sendCameraData(int x, int y, uint8_t size);	//#TODO send other camera values
+	void sendBatteryVoltage(float voltage);
+	void sendGyroOrientation(float z);
+	void sendStatus(uint8_t status, uint8_t substatus);
+	void sendRunning();
+	void sendStopped();
 	bool shouldStart();
 private:
     uint8_t calcChecksum(uint8_t packet[], int length);
