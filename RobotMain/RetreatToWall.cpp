e020c3a07479a7ee6cae5f40d1cd97caab09a047
hejohnson <hejohnson@wpi.edu>
2015-12-12 16:45:11 -0500

    Changed to turn back to position that it saw the candle at

--- RobotMain/RetreatToWall.cpp
+++ RobotMain/RetreatToWall.cpp
@@ -6,52 +6,22 @@ RetreatToWall::RetreatToWall(float setpoint) : Command("Retreat to Wall"){
 }
 
 void RetreatToWall::initialize() {
-
+  // calculate theta to new position
+  xDelta = ladder11->drivetrain->getCandleFoundX()-ladder11->drivetrain->getXOdoEst();
+  yDelta = ladder11->drivetrain->getCandleFoundY()-ladder11->drivetrain->getYOdoEst();
+  thetaDesired = tan(yDelta/xDelta);
+  // turn to that position
+  // drive to that position
 }
 
 void RetreatToWall::execute() {
- 
-
- 
- 
- if (ladder11->frontSensor->distance()<100) {
-    
-    distSpeed = .25*(ladder11->frontSensor->distance()-20);
-    if (distSpeed > 5) {
-    distSpeed = 5;
-    }
-    if (distSpeed < 1.5) {
-     distSpeed = 1.5;
-    }
-    ladder11->drivetrain->drive(distSpeed, 15);
-  } else {
-    ladder11->drivetrain->drive(0, 40);
-  }
-  
-  //Print distance on the lcd screen. 
-  ladder11->lcd->clear();
-  ladder11->lcd->print(ladder11->frontSensor->distance(), DEC);
-  //This part is just for the LEDs
-  if (getTime()%500 < 250) {
-    digitalWrite(8, LOW);
-    digitalWrite(9, LOW);
-    digitalWrite(32, HIGH);
-    digitalWrite(34, HIGH);
-    digitalWrite(46, HIGH);
-    digitalWrite(48, HIGH);
-  } else {
-    digitalWrite(8, HIGH);
-    digitalWrite(9, HIGH);
-    digitalWrite(32, LOW);
-    digitalWrite(34, LOW);
-    digitalWrite(46, LOW);
-    digitalWrite(48, LOW);
-  }
+  ladder11->drivetrain->drive(0, -30*(thetaDesired-ladder11->drivetrain->getOrientOdoEst()));
+  ladder11->drivetrain->updateRobotPos();
 }
 
 bool RetreatToWall::isFinished() {
 
-	return abs(ladder11->frontSensor->distance()<12);
+	return abs((thetaDesired-ladder11->drivetrain->getOrientOdoEst()))<10*3.14/180;
 }
 
 void RetreatToWall::end() {
