c9a6ee911bc35726fbb8512f4876f40fb4a73b8a
hejohnson <hejohnson@wpi.edu>
2015-12-12 20:31:33 -0500

    Converted to integer math
    We were getting very strange math errors with floating point
    calculations

--- RobotMain/RetreatToWall.cpp
+++ RobotMain/RetreatToWall.cpp
@@ -9,22 +9,38 @@ void RetreatToWall::initialize() {
   // calculate theta to new position
   xDelta = ladder11->drivetrain->getCandleFoundX()-ladder11->drivetrain->getXOdoEst();
   yDelta = ladder11->drivetrain->getCandleFoundY()-ladder11->drivetrain->getYOdoEst();
-  thetaDesired = tan(yDelta/xDelta);
+  thetaDesired = tan(xDelta/yDelta)*1000;
   // turn to that position
   // drive to that position
 }
 
 void RetreatToWall::execute() {
-  ladder11->drivetrain->drive(0, -30*(thetaDesired-ladder11->drivetrain->getOrientOdoEst()));
+  angleDelta = thetaDesired-(ladder11->drivetrain->getOrientOdoEst())*1000;
+  turnSpeed = -0.1*angleDelta;
+  if (turnSpeed > 0) {
+    if (turnSpeed < 25) {
+      turnSpeed = 25;
+    }
+  } else {
+    if (turnSpeed > -25) {
+      turnSpeed = -25;
+    }
+  }
+  ladder11->drivetrain->drive(0, -0.1*(angleDelta));
   ladder11->drivetrain->updateRobotPos();
 }
 
 bool RetreatToWall::isFinished() {
-
-	return abs((thetaDesired-ladder11->drivetrain->getOrientOdoEst()))<10*3.14/180;
+	return angleDelta<75 && angleDelta>-75;
 }
 
 void RetreatToWall::end() {
-	ladder11->lcd->print("Done wall");
-	ladder11->drivetrain->stop();
+  ladder11->drivetrain->stop();
+  ladder11->lcd->clear();
+  ladder11->lcd->setCursor(0, 0);
+	ladder11->lcd->print(thetaDesired, DEC);
+  ladder11->lcd->setCursor(0, 1);
+  ladder11->lcd->print(ladder11->drivetrain->getOrientOdoEst()*1000, DEC);
+  ladder11->lcd->setCursor(8, 1);
+  ladder11->lcd->print(angleDelta, DEC);
 }
