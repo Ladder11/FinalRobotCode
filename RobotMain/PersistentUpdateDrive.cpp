b1d8cfff141d6358b6fb44c82d2d294986dd4bdc
Hans Johnson <hejohnson@wpi.edu>
2015-12-15 02:30:00 -0500

    Converted drivetrain odo angle to degrees before transmitting

--- RobotMain/PersistentUpdateDrive.cpp
+++ RobotMain/PersistentUpdateDrive.cpp
@@ -21,7 +21,7 @@ void PersistentUpdateDrive::execute() {
 	ladder11->drivetrain->updateRobotPos();
 	ladder11->gyro->updateGyro();
 	if (getTime() > triggerTime) {
-		ladder11->telemetry->sendRobotPose(ladder11->drivetrain->getXOdoEst(), ladder11->drivetrain->getYOdoEst(), ladder11->drivetrain->getOrientOdoEst());
+		ladder11->telemetry->sendRobotPose(ladder11->drivetrain->getXOdoEst(), ladder11->drivetrain->getYOdoEst(), ladder11->drivetrain->getOrientOdoEst()*180/3.1415);
 		ladder11->telemetry->sendGyroOrientation(ladder11->gyro->getGyro());
 		triggerTime = getTime() + 100;
 	}
